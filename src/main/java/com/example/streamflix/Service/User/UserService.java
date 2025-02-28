0c712e99e7357b5916089fccb0046482a7b91471
Sharan <sharan11302@gmail.com>
2025-02-28 10:01:58 +0530

    Update UserService.java

--- src/main/java/com/example/streamflix/Service/User/UserService.java
+++ src/main/java/com/example/streamflix/Service/User/UserService.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface UserService {
     String registerUser(String name, String email, String password, Role role);
     String loginUser(String name, String password);
-    List<UserDto> getAllUsers();  // New method to fetch all users
-    void deleteUser(Long id);     // New method to delete a user
+    List<UserDto> getAllUsers();  
+    void deleteUser(Long id);     
 }
